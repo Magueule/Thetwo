c613cfb3eaa7e00618f44f72011e2118ec43fcb7
Magueule <toan94@hotmail.com>
2016-06-16 18:09:42 +0200

    Initial commit

---
+++ app/src/main/java/com/muddapeople/thetwo/section1.java
@@ -0,0 +1,22 @@
+package com.muddapeople.thetwo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by antoine on 14/06/2016.
+ */
+public class section1 extends Fragment{
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        rootview = inflater.inflate(R.layout.section1_layout,container, false);
+        return rootview;
+    }
+}
